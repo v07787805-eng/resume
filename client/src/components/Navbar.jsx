e9c81f5b0d4eb4de0087d3d101c6f83e270d42da
RAM <v07787805@gmail.com>
2026-01-02 19:18:44 +0530

    Fix logo import in Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
 import { logout } from '../app/features/authSlice.js';
+import logo from '../assets/logo.svg'
+
 const Navbar = () => {
 
   const {user} = useSelector(state => state.auth)
@@ -16,7 +18,7 @@ return (
     <div className='shadow bg-white'>
     <nav className='flex items-center justify-between max-w-7x1 mx-auto px-4 py-3.5 text-slate-800 transition-all'>
     <Link to='/'>
-        <img src="../src/assets/logo.svg" alt="logo" className="h-11 w-auto" />
+        <img src={logo} alt="logo" className="h-11 w-auto" />
     </Link>
     <div className='flex items-center gap-4 text-sm'>
       <p className='max-sm:hidden'>Hi, {user?.name}</p>
